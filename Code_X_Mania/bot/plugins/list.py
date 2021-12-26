d27c65ed542cbbc5daa85eb7355f1fb0e9f1a34e
akprivatebots <90455659+akprivatebots@users.noreply.github.com>
2021-12-26 22:32:07 +0530

    Update list.py

--- Code_X_Mania/bot/plugins/list.py
+++ Code_X_Mania/bot/plugins/list.py
@@ -7,7 +7,6 @@ def _start(client, message):
     client.send_message(chat_id = message.chat.id,
         text = LIST_MSG.format(message.from_user.mention),
         reply_to_message_id = message.message_id,
-        parse_mode="markdown"
-    )
+        parse_mode="markdown")
     
-    LIST_MSG = "Hi! {} Here is a list of all my commands \n \n 1 . `start⚡️` \n 2. `help📚` \n 3. `login🔑` \n 4.`follow❤️` \n 5. `ping📡` \n 6. `status📊` \n 7. `dc` this tells your telegram dc \n 8. `maintainers😎` "
+    LIST_MSG = "Hi! {first} Here is a list of all my commands \n \n 1 . `start⚡️` \n 2. `help📚` \n 3. `login🔑` \n 4. `ping📡` \n 5. `status📊` \n 6. `dc` this tells your telegram dc \n 8. `maintainers😎` "
